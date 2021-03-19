906e768b5cfb288da8bd3e3933719e60ed9ce8c3
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-03-19 21:38:34 +0000

    Update Network.java

--- Network.java
+++ Network.java
@@ -25,8 +25,6 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.*;
 
 public class Network implements Runnable {
-    public static Map<String, Double> failureThreads = new ConcurrentHashMap<>(3);
-    BlockingQueue<String> waiting_Queue = new LinkedBlockingDeque<>();
 
     BlockingQueue<String> FAST = new LinkedBlockingDeque<>();
     BlockingQueue<String> MEDIUM = new LinkedBlockingDeque<>();
@@ -35,6 +33,34 @@ public class Network implements Runnable {
     private Lock lock = new ReentrantLock();
     List<Double> bandwidth= Arrays.asList(0.02, 0.2, 2.0);
 
+    public void getQueue(String name, double networkspeed){
+        if (networkspeed == 2.0){
+            FAST.add(name);
+        }
+        if (networkspeed == 0.2){
+            MEDIUM.add(name);
+        }
+        if (networkspeed == 0.02){
+            SLOW.add(name);
+        }
+    }
+
+    public String checkSpeed(String name){
+        if(FAST.contains(name)){
+            System.out.println("Network speed @ 2.0 -- FAST network connection ");
+            return "FAST";
+        }
+        if(MEDIUM.contains(name)){
+          System.out.println("Network speed @ 0.2 -- MEDIUM network connection");
+            return "MEDIUM";
+
+        }
+        else{
+            System.out.println("Network speed @ 0.02 -- SLOW network connection");
+            return "SLOW";
+
+        }
+    }
 
     public double getSpeed(){
         Random rand = new Random();
@@ -42,39 +68,65 @@ public class Network implements Runnable {
         return randomElement;
     }
 
-    public void getQueue(){
-        System.out.println("Failure Threads: " + failureThreads);
-        System.out.println("Waiting Queue: " + waiting_Queue);
+    public void sleep(int updateDevLength, int speed){
+        try {
+            Thread.sleep(updateDevLength * speed);
+        }
+        catch (InterruptedException e){
+
+            System.out.println("Update Development has been interrupted");
+
+        }
     }
 
-    public int developSoftwareUpdate(){
+    public void developSoftwareUpdate(String name){
         Random rand = new Random();
-        return (rand.nextInt(9-2) + 2);
-    }
+        int updateDevLength = (rand.nextInt(9-2) + 2);
+        String networkq = checkSpeed(name);
+        if (networkq == "FAST") {
+            sleep(updateDevLength, 2000);
+        }
 
-    public int initializeSoftwareUpdate(){
+        if(networkq == "MEDIUM") {
+            sleep(updateDevLength, 4000);
+        }
+
+        if (networkq == "SLOW"){
+            sleep(updateDevLength, 6000);
+    }}
+
+    public void initializeSoftwareUpdate(String name){
         Random rand = new Random();
-        return (rand.nextInt(9-2) + 2);
+        int updateLength = (rand.nextInt(9-2) + 2);
+        String networkq = checkSpeed(name);
+        System.out.println("Updating..");
+        if (networkq == "FAST") {
+            sleep(updateLength, 2000);
+        }
+
+        if(networkq == "MEDIUM") {
+            sleep(updateLength, 4000);
+        }
+
+        if (networkq == "SLOW") {
+            sleep(updateLength, 60000);
+        }
+    }
+    public BlockingQueue<String> fastNetwork = new LinkedBlockingDeque<>();
+
+    public void componentsQueue(String name,HashMap<String,List> hash) {
+
+        List components = hash.get(name);
+
+        for (int i = 0; i < components.size(); i++) {
+            double speed = 2.0;
+
+            if (speed == 2.0){
+                System.out.println(components.get(i));
+            }
+        }
     }
 
-    public synchronized void failure(){ //changed to synchronised as threads where reading at the same time.
-        double x = getSpeed(); //get a random network speed
-        System.out.println(Thread.currentThread().getName() + " " + x );
-        if(!failureThreads.containsValue(x)){ //if the network speed is not in use, add the thread and speed to the hashmap.
-            failureThreads.put(Thread.currentThread().getName(), x);
-        }
-        else{
-          waiting_Queue.add(Thread.currentThread().getName());
-            for (double i : bandwidth) {
-                       if (!failureThreads.containsValue(i) && (failureThreads.size() <= 3)) {
-                          System.out.println(Thread.currentThread().getName() + " failureThread doesnt contain " + i);
-                            failureThreads.put(Thread.currentThread().getName(), i);
-                        }
-                   }
-        }
-
-        }
-
-    public void run(){
+        public void run(){
     }
 }
