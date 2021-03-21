08d795d6e5686c18b61e002fb921ddd8f99e9787
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-03-21 17:19:20 +0000

    Update Network.java

--- Network.java
+++ Network.java
@@ -1,54 +1,45 @@
 import java.util.concurrent.BlockingQueue;
 import java.util.Random;
 import java.util.concurrent.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.*;
 
 public class Network{
-
-    BlockingQueue<String> FAST = new LinkedBlockingDeque<>();
-    BlockingQueue<String> MEDIUM = new LinkedBlockingDeque<>();
-    BlockingQueue<String> SLOW = new LinkedBlockingDeque<>();
-
-    boolean sendToController = false;
-    boolean sendToMission = false;
-
     BlockingQueue<String> reports = new LinkedBlockingDeque<>();
+    boolean avalbility = false;
+    int updateSize;
 
-
-
-    private Lock lock = new ReentrantLock();
     List<Double> bandwidth= Arrays.asList(0.02, 0.2, 2.0);
 
-    public void getQueue(String name, double networkspeed){
-        if (networkspeed == 2.0){
-            FAST.add(name);
+    public void sleep(double speed){
+        if (speed == 2.0 ){
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+
+                System.out.println("has been interrupted");
+
+            }
         }
-        if (networkspeed == 0.2){
-            MEDIUM.add(name);
+        if (speed == 0.2 ){
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+
+                System.out.println("has been interrupted");
+
+            }
         }
-        if (networkspeed == 0.02){
-            SLOW.add(name);
+        if (speed == 0.02 ){
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+
+                System.out.println("has been interrupted");
+
+            }
         }
     }
 
-    public String checkSpeed(String name){
-        if(FAST.contains(name)){
-            System.out.println("Network speed @ 2.0 -- FAST network connection ");
-            return "FAST";
-        }
-        if(MEDIUM.contains(name)){
-          System.out.println("Network speed @ 0.2 -- MEDIUM network connection");
-            return "MEDIUM";
-
-        }
-        else{
-            System.out.println("Network speed @ 0.02 -- SLOW network connection");
-            return "SLOW";
-
-        }
-    }
 
     public double getSpeed(){
         Random rand = new Random();
@@ -56,83 +47,51 @@ public class Network{
         return randomElement;
     }
 
-    public void sleep(int updateDevLength, int speed){
+    public void SoftwareUpdateAvailable(int US){
+        avalbility = true;
+        updateSize = US;
+
+    }
+
+    public int developSoftwareUpdate(){
+        Random rand = new Random();
+        int updateDevLength = (rand.nextInt(9-2) + 2);
+        int bits = (rand.nextInt(9-2) + 2);
         try {
-            Thread.sleep(updateDevLength * speed);
+            Thread.currentThread().sleep(updateDevLength * 2000);
         }
         catch (InterruptedException e){
 
             System.out.println("Update Development has been interrupted");
 
         }
+        return bits;
     }
 
-    public void developSoftwareUpdate(String name){
-        Random rand = new Random();
-        int updateDevLength = (rand.nextInt(9-2) + 2);
-        String networkq = checkSpeed(name);
-        if (networkq == "FAST") {
-            sleep(updateDevLength, 2000);
-        }
-
-        if(networkq == "MEDIUM") {
-            sleep(updateDevLength, 4000);
-        }
-
-        if (networkq == "SLOW"){
-            sleep(updateDevLength, 6000);
-    }}
-
-    public void initializeSoftwareUpdate(String name){
-        Random rand = new Random();
-        int updateLength = (rand.nextInt(9-2) + 2);
-        String networkq = checkSpeed(name);
-        System.out.println("Updating..");
-        if (networkq == "FAST") {
-            sleep(updateLength, 2000);
-        }
-
-        if(networkq == "MEDIUM") {
-            sleep(updateLength, 4000);
-        }
-
-        if (networkq == "SLOW") {
-            sleep(updateLength, 60000);
-        }
-    }
-
-//    public void componentsQueue(String name,HashMap<String,List> hash) {
-//
-//        List components = hash.get(name);
-//
-//        for (int i = 0; i < components.size(); i++) {
-//            double speed = 2.0;
-//
-//            if (speed == 2.0){
-//                System.out.println(components.get(i));
-//            }
-//        }
-//    }
-    public void AddComponentReportToControllerQ(String name, double networkSpeed , Object component, String message, int messageSize){
+    public void AddComponentReportToControllerQ(String name, double networkSpeed , Object component, String message, MissionController controller){
         System.out.println(name + " Component: " + component + " makes request to network: " + networkSpeed + " at time: " + " for message: " + message);
-//        reports.add(message); // add report to queue
-//        SendReportToController();
+        reports.add(message); // add report to queue
+        SendReportToController(name, controller, networkSpeed);
 
     }
-    public void AddReportToControllerQ(String name, double networkSpeed , String messageType, int messageSize){
+    public void AddReportToControllerQ(String name, double networkSpeed , String messageType, int messageSize, MissionController controller){
         Report re = new Report();
         String message = re.Report(messageType, messageSize);
         System.out.println(name + " makes request to network: " + networkSpeed + " at time: " + " for message: " + message);
         reports.add(message); // add report to queue.
-        SendReportToController();
+        SendReportToController(name, controller, networkSpeed);
     }
 
-    public void SendReportToController(){
+    public void SendReportToController(String name, MissionController controller, double networkSpeed){
         String message = reports.poll(); //.poll gets the first report from the queue and removes it.
-        MissionController mc = new MissionController(0);
-        mc.recieveReport(message); //send report to mission controller
+        sleep(networkSpeed);
+        controller.recieveReport(name, message, this, networkSpeed);
+        //send report to mission controller
     }
 
-
-
+    public String SendReportToMission(String name, double networkSpeed, String message){
+        message = (name + " makes request to network: " + networkSpeed + " at time: " + " for message: " + message);
+        sleep(networkSpeed);
+        return message;
     }
+}
