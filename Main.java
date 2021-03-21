d8a03ede0653e0cf35b6d02979ab4280a90e5da6
Munashe Lawrence Chifungo <munashe.chifungo2@mail.dcu.ie>
2021-03-21 20:15:54 +0000

    Upload New File

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+// Mission Controller
+public class Main {
+
+    public static void main(String[] args) {
+
+        // initialize mission controller
+
+        MissionController controller = new MissionController(0);
+
+        controller.start();
+
+    }
+}
+
