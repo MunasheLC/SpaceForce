9f6a6d76391f1b97e578acb060a442051af67c72
Fiona Tuite <fiona.tuite2@mail.dcu.ie>
2021-03-21 22:06:09 +0000

    Update Report.java

--- Report.java
+++ Report.java
@@ -5,10 +5,10 @@ public class Report{
     HashMap<String, String> messages = new HashMap<>();
 
     public void addToMap(){
-        messages.put("SOFTWAREUPDATE"," URGENT - Software Update needed.");
-        messages.put("UPDATESUCCESS"," SUCCESS - Software Update Success.");
-        messages.put("TERMINATE", " URGENT - Terminate mission.");
-        messages.put("FAIL", " WARNING - Failing.");
+        messages.put("SOFTWAREUPDATE","URGENT - Software Update needed.");
+        messages.put("UPDATESUCCESS","SUCCESS - Software Update Success.");
+        messages.put("TERMINATE", "URGENT - Terminate mission.");
+        messages.put("FAIL", "WARNING - Failing.");
         messages.put("TELEMETRY", "tel");
         messages.put("DATA", "data");
 
